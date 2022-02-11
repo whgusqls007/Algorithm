16a42399b92b37c1731e79bab3231fa59abc9103
whgusqls007 <54840668+whgusqls007@users.noreply.github.com>
2022-02-11 16:51:36 +0900

    [Silver V] Title: 요세푸스 문제, Time: 308 ms, Memory: 16768 KB -BaekjoonHub

---
+++ 백준/Silver/1158.요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader      br    = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer     st    = new StringTokenizer(br.readLine());
+		StringBuilder       sb    = new StringBuilder();
+		LinkedList<Integer> ll    = new LinkedList<>();
+		int                 N     = Integer.parseInt(st.nextToken());
+		int                 K     = Integer.parseInt(st.nextToken());
+		int                 index = 0;
+		int                 cnt   = 0;
+
+		for (int i = 0; i < N; i++) {
+			ll.add(i + 1);
+		}
+		sb.append("<");
+		while (ll.size() != 0) {
+			cnt++;
+			if (cnt % K == 0) {
+				sb.append(ll.get(index)).append(", ");
+				ll.remove(index % N);
+				cnt = 0;
+			} else {
+				index++;
+			}
+			if (index >= ll.size()) {
+				index = 0;
+			}
+		}
+		sb.setLength(sb.length() - 2);
+		sb.append(">");
+		System.out.println(sb);
+	}
+}

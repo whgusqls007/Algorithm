3135386090cef1f069cf98db9530767a8b301500
whgusqls007 <54840668+whgusqls007@users.noreply.github.com>
2022-02-11 17:06:23 +0900

    [Silver V] Title: 요세푸스 문제, Time: 248 ms, Memory: 16968 KB -BaekjoonHub

--- 백준/Silver/1158.요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158.요세푸스 문제/요세푸스 문제.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
@@ -9,28 +10,36 @@ public class Main {
 		StringTokenizer     st    = new StringTokenizer(br.readLine());
 		StringBuilder       sb    = new StringBuilder();
 		LinkedList<Integer> ll    = new LinkedList<>();
+		ArrayDeque<Integer> dq    = new ArrayDeque<>();
 		int                 N     = Integer.parseInt(st.nextToken());
 		int                 K     = Integer.parseInt(st.nextToken());
 		int                 index = 0;
 		int                 cnt   = 0;
 
 		for (int i = 0; i < N; i++) {
-			ll.add(i + 1);
+			// ll.add(i + 1);
+			dq.offerLast(i + 1);
 		}
 		sb.append("<");
-		while (ll.size() != 0) {
-			cnt++;
-			if (cnt % K == 0) {
-				sb.append(ll.get(index)).append(", ");
-				ll.remove(index % N);
-				cnt = 0;
-			} else {
-				index++;
+		while (!dq.isEmpty()) {
+			for (int i = 0; i < K - 1; i++) {
+				dq.offerLast(dq.pollFirst());
 			}
-			if (index >= ll.size()) {
-				index = 0;
-			}
+			sb.append(dq.pollFirst()).append(", ");
 		}
+		// while (ll.size() != 0) {
+		// cnt++;
+		// if (cnt % K == 0) {
+		// sb.append(ll.get(index)).append(", ");
+		// ll.remove(index % N);
+		// cnt = 0;
+		// } else {
+		// index++;
+		// }
+		// if (index >= ll.size()) {
+		// index = 0;
+		// }
+		// }
 		sb.setLength(sb.length() - 2);
 		sb.append(">");
 		System.out.println(sb);

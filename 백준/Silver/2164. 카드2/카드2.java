2c724266763287f01f1d54fffa0956caaed6a830
whgusqls007 <54840668+whgusqls007@users.noreply.github.com>
2022-03-07 21:35:37 +0900

    [Silver IV] Title: 카드2, Time: 144 ms, Memory: 43164 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -12,15 +12,20 @@ public class Main {
 		for (int i = 0; i < N; i++) {
 			q.offer(i + 1);
 		}
-
-		while (q.size() > 1) {
+		if (q.size() == 1) {
+			System.out.println(q.poll());
+			return;
+		}
+		while (true) {
 			// 카드 한장 버림
 			q.poll();
 
+			if (q.size() == 1) {
+				System.out.println(q.poll());
+				return;
+			}
 			// 그 다음 카드를 골라서 맨 밑으로 옮김
 			q.offer(q.poll());
 		}
-
-		System.out.println(q.poll());
 	}
 }

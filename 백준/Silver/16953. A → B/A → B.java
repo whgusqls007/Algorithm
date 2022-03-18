d8635eafd0c2d629f7c93886ca82d3f797c45ac2
whgusqls007 <54840668+whgusqls007@users.noreply.github.com>
2022-03-18 20:30:43 +0900

    [Silver I] Title: A → B, Time: 88 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int min = Integer.MAX_VALUE;
+
+	public static void main( String[] args ) throws Exception {
+		StringTokenizer st = new StringTokenizer(
+				new BufferedReader( new InputStreamReader( System.in ) ).readLine()
+		);
+
+		dfs( Long.parseLong( st.nextToken() ), Long.parseLong( st.nextToken() ), 1 );
+		System.out.println( min == Integer.MAX_VALUE ? -1 : min );
+	}
+
+	public static void dfs( long current, long target, int count ) {
+		if ( current > target ) {
+			return;
+		} else {
+			if ( current * 2 == target ) {
+				min = Math.min( min, count + 1 );
+			} else if ( current * 10 + 1 == target ) {
+				min = Math.min( min, count + 1 );
+			} else {
+				dfs( current * 2, target, count + 1 );
+				dfs( current * 10 + 1, target, count + 1 );
+			}
+		}
+	}
+}

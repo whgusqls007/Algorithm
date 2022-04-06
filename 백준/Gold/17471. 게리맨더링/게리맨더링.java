bd6f2cfba118a5d596153b74766ceb8b490a3963
whgusqls007 <54840668+whgusqls007@users.noreply.github.com>
2022-04-06 14:46:53 +0900

    [Gold IV] Title: 게리맨더링, Time: 232 ms, Memory: 11944 KB -BaekjoonHub

---
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -0,0 +1,182 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class City {
+	int             number;
+	int             population;
+	boolean         RB;
+	List< Integer > nextCity;
+
+	public City( int number, int population ) {
+		super();
+		this.number     = number;
+		this.population = population;
+		this.nextCity   = new ArrayList<>();
+		this.RB         = false;
+	}
+
+}
+
+public class Main {
+	static BufferedReader br;
+	static int            N;
+	static List< City >   cities;
+	static int[][]        map;
+	static boolean[]      isBlue;
+	static boolean[]      isRed;
+	static boolean[]      visit;
+	static int            min;
+
+	public static void main( String[] args ) throws NumberFormatException, IOException {
+		init();
+
+		selectRedBlue( 0, 0 );
+
+		System.out.println( min == Integer.MAX_VALUE ? -1 : min );
+	}
+
+	static void selectRedBlue( int index, int num ) {
+		// 선택을 다 했으면
+		if ( index == N ) {
+
+			// 모두 빨간색이거나 모두 파란색이면 안됨
+			if ( !checkColor( num ) ) return;
+
+			// 색이 섞여 있다면 dfs로 연결돼 있는지 확인
+			if ( checkConnection( num ) ) {
+				// 확인했으면 시민들 차이 구하기
+				min = Math.min( min, getPopSub( num ) );
+			}
+
+			return;
+		}
+
+		// index 마을이 파란색일때
+		isBlue[index] = true;
+		selectRedBlue( index + 1, num | ( 1 << index ) );
+
+		// index 마을이 빨간색일때
+		isBlue[index] = false;
+		selectRedBlue( index + 1, num );
+	}
+
+	static int getPopSub( int num ) {
+		int bp = 0;
+		int rp = 0;
+		for ( int i = 0; i < N; i++ ) {
+			if ( ( num & 1 << i ) != 0 ) {
+				bp += cities.get( i ).population;
+			} else {
+				rp += cities.get( i ).population;
+			}
+		}
+		return Math.abs( bp - rp );
+	}
+
+	static boolean checkConnection( int num ) {
+		int r = 0;
+		int b = 0;
+
+		for ( int i = 0; i < N; i++ ) {
+			if ( ( num & ( 1 << i ) ) != 0 ) {
+				b = checkBlueConnection( i, 1 << i, num );
+				break;
+			}
+		}
+
+		for ( int i = 0; i < N; i++ ) {
+			if ( ( num & ( 1 << i ) ) == 0 ) {
+				r = checkRedConnection( i, 1 << i, num );
+				break;
+			}
+		}
+
+		if ( r + b == ( 1 << N ) - 1 ) {
+			return true;
+		}
+		return false;
+	}
+
+	static int checkRedConnection( int index, int bit, int num ) {
+		int newBit = 0;
+		for ( int j = 0; j < N; j++ ) {
+			// index와 j 가 연결 되어 있고
+			if ( map[index][j] != 0 ) {
+				// j 가 빨간색이고
+				if ( ( num & ( 1 << j ) ) == 0 ) {
+					// 아직 방문 안했을 때
+					if ( ( bit & ( 1 << j ) ) == 0 ) {
+						newBit |= bit | checkRedConnection( j, bit | ( 1 << j ), num );
+					}
+				}
+			}
+		}
+
+		return newBit == 0 ? bit : newBit;
+	}
+
+	static int checkBlueConnection( int index, int bit, int num ) {
+		int newBit = 0;
+		for ( int j = 0; j < N; j++ ) {
+			// index와 j 가 연결 되어 있고
+			if ( map[index][j] != 0 ) {
+				// j 가 파란색이고
+				if ( ( num & ( 1 << j ) ) != 0 ) {
+					// 아직 방문 안했을 때
+					if ( ( bit & ( 1 << j ) ) == 0 ) {
+						newBit |= bit | checkBlueConnection( j, bit | ( 1 << j ), num );
+					}
+				}
+			}
+		}
+
+		return newBit == 0 ? bit : newBit;
+	}
+
+	static boolean checkColor( int num ) {
+		if ( num == ( 1 << N ) - 1 ) {
+			return false;
+		} else if ( num == 0 ) {
+			return false;
+		}
+		return true;
+	}
+
+	static void init() throws NumberFormatException, IOException {
+		br     = new BufferedReader( new InputStreamReader( System.in ) );
+		N      = Integer.parseInt( br.readLine() );
+		cities = new ArrayList< City >();
+		map    = new int[N][N];
+		isBlue = new boolean[N];
+		isRed  = new boolean[N];
+		min    = Integer.MAX_VALUE;
+
+		StringTokenizer st = new StringTokenizer( br.readLine() );
+
+		for ( int i = 0; i < N; i++ ) {
+			cities.add(
+					new City(
+							i,
+							Integer.parseInt( st.nextToken() )
+					)
+			);
+		}
+
+		for ( int i = 0; i < N; i++ ) {
+			st = new StringTokenizer( br.readLine() );
+			int n = Integer.parseInt( st.nextToken() );
+
+			for ( int j = 0; j < n; j++ ) {
+				int num = Integer.parseInt( st.nextToken() );
+				cities.get( i ).nextCity.add( num - 1 );
+				map[i][num - 1] = 1;
+			}
+		}
+
+		br.close();
+	}
+}

48565d505797f59588b6b08f9047406616a2bb3f
whgusqls007 <54840668+whgusqls007@users.noreply.github.com>
2022-07-16 16:07:22 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 624 ms, Memory: 31796 KB -BaekjoonHub

---
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+class Snowman {
+	int x1;
+	int x2;
+	int sum;
+
+	public Snowman(int x1, int x2, int sum) {
+		super();
+		this.x1 = x1;
+		this.x2 = x2;
+		this.sum = sum;
+	}
+
+	@Override
+	public String toString() {
+		return "Snowman [x1=" + x1 + ", x2=" + x2 + ", sum=" + sum + "]";
+	}
+
+}
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		List<Snowman> list = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				list.add(new Snowman(i, j, arr[i] + arr[j]));
+			}
+		}
+
+		Collections.sort(list, (e1, e2) -> Integer.compare(e1.sum, e2.sum));
+//		list.forEach(e -> System.out.println(e));
+
+		int min = Integer.MAX_VALUE;
+
+		for (int i = 0; i < list.size() - 1; i++) {
+
+			if (list.get(i).x1 == list.get(i + 1).x1) {
+				continue;
+			}
+
+			if (list.get(i).x1 == list.get(i + 1).x2) {
+				continue;
+			}
+
+			if (list.get(i).x2 == list.get(i + 1).x1) {
+				continue;
+			}
+
+			if (list.get(i).x2 == list.get(i + 1).x2) {
+				continue;
+			}
+
+			min = Math.min(min, Math.abs(list.get(i).sum - list.get(i + 1).sum));
+		}
+
+		System.out.println(min);
+	}
+}

e58e5509f1a2fda76ec15c0f187c11d5eb6a8aa6
Hyunbin Jo <54840668+whgusqls007@users.noreply.github.com>
2023-04-12 16:12:58 +0900

    [unrated] Title: 카드 뭉치, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "Yes";
+        boolean[] used1 = new boolean[cards1.length];
+        boolean[] used2 = new boolean[cards2.length];
+        
+        int index1 = 0;
+        int index2 = 0;
+        
+        for(int i = 0; i < goal.length; i++) {
+            if(index1 < cards1.length && cards1[index1].equals(goal[i])) {
+                index1++;
+            } else if(index2 < cards2.length && cards2[index2].equals(goal[i])) {
+                index2++;
+            } else {
+                answer = "No";
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}

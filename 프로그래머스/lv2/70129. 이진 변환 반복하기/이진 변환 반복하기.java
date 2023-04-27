a06b7525a2e3b204dba0a9698097d630fcb8cd10
Hyunbin Jo <54840668+whgusqls007@users.noreply.github.com>
2023-04-27 20:21:13 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 44.27 ms, Memory: 98.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+
+        while (!s.equals("1")) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '0') {
+                    answer[1]++;
+                }
+            }
+
+            s = s.replaceAll("0", "");
+
+            s = Integer.toString(s.length(), 2);
+            answer[0]++;
+        }
+
+        return answer;
+    }
+}

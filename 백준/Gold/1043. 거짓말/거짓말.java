7a2318c63472ff67c5638311a53fa8064fedf7cf
Hyunbin Jo <54840668+whgusqls007@users.noreply.github.com>
2023-08-25 23:28:12 +0900

    [Gold IV] Title: 거짓말, Time: 164 ms, Memory: 16336 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,92 +1,81 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int[]                         parent;
-	static int                           N;
-	static int                           M;
-	static int[]                         truth = null;
-	static ArrayList<ArrayList<Integer>> party = new ArrayList<ArrayList<Integer>>();
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+    int[] arr = new int[N + 1];
+    int cnt = M;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter  bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N + 1; i++) {
+      arr[i] = i;
+    }
 
-		N  = Integer.parseInt(st.nextToken());
-		M  = Integer.parseInt(st.nextToken());
+    st = new StringTokenizer(br.readLine());
 
-		st = new StringTokenizer(br.readLine());
+    int a = Integer.parseInt(st.nextToken());
+    int min = 0;
+    if (a == 0) {
+      System.out.println(M);
+      System.exit(0);
+    } else if (a >= 1) {
+      for (int i = 0; i < a; i++) {
+        int b = Integer.parseInt(st.nextToken());
+        arr[b] = 0;
+      }
+    }
 
-		int a = Integer.parseInt(st.nextToken());
+    ArrayList<ArrayList<Integer>> list = new ArrayList<>();
 
-		if (a != 0) {
-			truth = new int[a + 1];
+    for (int i = 0; i < M; i++) {
+      list.add(new ArrayList<>());
+    }
 
-			for (int i = 1; i <= a; i++) {
-				truth[i] = Integer.parseInt(st.nextToken());
-			}
+    for (int i = 0; i < M; i++) {
+      st = new StringTokenizer(br.readLine());
+      while (st.hasMoreTokens()) {
+        list.get(i).add(Integer.parseInt(st.nextToken()));
+      }
+    }
 
-		} else {
-			System.out.println(M);
-			return;
-		}
+    for (int i = 0; i < M; i++) {
+      int n = list.get(i).get(0);
+      for (int j = 1; j < n; j++) {
+        union(arr, list.get(i).get(j), list.get(i).get(j + 1));
+      }
+    }
 
-		for (int i = 0; i < M + 2; i++) {
-			party.add(new ArrayList<Integer>());
-		}
+    continuePoint: for (int i = 0; i < M; i++) {
+      int n = list.get(i).get(0);
+      for (int j = 1; j < n + 1; j++) {
+        if (find(arr, list.get(i).get(j)) == 0) {
+          cnt--;
+          continue continuePoint;
+        }
+      }
+    }
 
-		for (int i = 1; i <= M; i++) {
-			st = new StringTokenizer(br.readLine());
-			a  = Integer.parseInt(st.nextToken());
-			for (int j = 1; j <= a; j++) {
-				party.get(i).add(Integer.parseInt(st.nextToken()));
-			}
-		}
+    System.out.println(cnt);
+  }
 
-		makeSet();
+  public static void union(int[] arr, int a, int b) {
+    a = find(arr, a);
+    b = find(arr, b);
 
-		for (int i = 1; i <= M; i++) {
-			int current = party.get(i).get(0);
-			for (int j = 0; j < party.get(i).size(); j++) {
-				union(current, party.get(i).get(j));
-			}
-		}
+    if (a <= b) {
+      arr[b] = a;
+    } else {
+      arr[a] = b;
+    }
+  }
 
-		int ans = 0;
-
-		for (int i = 1; i <= M; i++) {
-			boolean tell    = true;
-			int     current = party.get(i).get(0);
-			for (int j = 1; j < truth.length; j++) {
-				if (findSet(current) == findSet(truth[j])) {
-					tell = false;
-				}
-			}
-			if (tell) ans++;
-		}
-
-		System.out.println(ans);
-	}
-
-	static void makeSet() {
-		parent = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
-			parent[i] = i;
-		}
-	}
-
-	static int findSet(int x) {
-		return parent[x] = parent[x] == x ? x : findSet(parent[x]);
-	}
-
-	static void union(int x, int y) {
-		int px = findSet(x);
-		int py = findSet(y);
-		parent[py] = px;
-	}
+  public static int find(int[] arr, int a) {
+    if (arr[a] == a) {
+      return a;
+    }
+    return arr[a] = find(arr, arr[a]);
+  }
 }

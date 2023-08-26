59a52a576aa3b08841d3010ae9f045b7cc426ff8
Hyunbin Jo <54840668+whgusqls007@users.noreply.github.com>
2023-08-26 22:23:36 +0900

    [Gold IV] Title: 플로이드, Time: 352 ms, Memory: 42408 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,71 +1,53 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int       N, M;
-	static int[][]   buses;
-	static int[][]   matrix;
-	static final int INF = 100_000_000;
+  static int INF = 100_000_000;
 
-	public static void main( String[] args ) throws NumberFormatException, IOException {
-		init();
-		floydWarshall();
-		print();
-	}
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int M = Integer.parseInt(br.readLine());
 
-	static void floydWarshall() {
-		for ( int k = 1; k <= N; k++ ) { // 경
-			for ( int i = 1; i <= N; i++ ) { // 출
-				if ( k == i ) continue;
-				for ( int j = 1; j <= N; j++ ) { // 도
-					if ( i == j ) continue;
-					if ( matrix[i][k] == INF || matrix[k][j] == INF ) continue;
+    int[][] arr = new int[N + 1][N + 1];
+    for (int i = 0; i < arr.length; i++) {
+      Arrays.fill(arr[i], INF);
+    }
 
-					matrix[i][j] = Math.min( matrix[i][k] + matrix[k][j], matrix[i][j] );
-				}
-			}
-		}
-	}
+    for (int i = 0; i < M; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      int c = Integer.parseInt(st.nextToken());
 
-	static void print() throws IOException {
-		BufferedWriter bw = new BufferedWriter( new OutputStreamWriter( System.out ) );
-		for ( int i = 1; i <= N; i++ ) {
-			for ( int j = 1; j <= N; j++ ) {
-				if ( matrix[i][j] == INF ) bw.write( "0 " );
-				else bw.write( String.format( "%d ", matrix[i][j] ) );
-			}
-			bw.write( "\n" );
-		}
-		bw.flush();
-	}
+      arr[a][b] = Math.min(arr[a][b], c);
+    }
 
-	static void init() throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
-		N      = Integer.parseInt( br.readLine() );
-		M      = Integer.parseInt( br.readLine() );
-		matrix = new int[101][101];
+    for (int i = 1; i < N + 1; i++) {
+      for (int j = 1; j < N + 1; j++) {
+        if (i == j) {
+          continue;
+        }
 
-		for ( int i = 0; i < M; i++ ) {
-			StringTokenizer st         = new StringTokenizer( br.readLine() );
-			int             departCity = Integer.parseInt( st.nextToken() );
-			int             arriveCity = Integer.parseInt( st.nextToken() );
-			int             cost       = Integer.parseInt( st.nextToken() );
+        for (int k = 1; k < N + 1; k++) {
+          if (j == k || arr[j][i] == INF || arr[i][k] == INF) {
+            continue;
+          }
 
-			if ( matrix[departCity][arriveCity] != 0 ) {
-				matrix[departCity][arriveCity] = Math.min( matrix[departCity][arriveCity], cost );
-			} else {
-				matrix[departCity][arriveCity] = cost;
-			}
-		}
+          arr[j][k] = Math.min(arr[j][k], arr[j][i] + arr[i][k]);
+        }
+      }
+    }
 
-		for ( int i = 1; i <= N; i++ ) {
-			for ( int j = 1; j <= N; j++ ) {
-				if ( i != j && matrix[i][j] == 0 ) matrix[i][j] = INF;
-			}
-		}
-	}
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    for (int i = 1; i < N + 1; i++) {
+      for (int j = 1; j < N + 1; j++) {
+        bw.write(arr[i][j] == INF ? 0 + " " : arr[i][j] + " ");
+      }
+      bw.write("\n");
+    }
+
+    bw.flush();
+  }
 }
